9c9a8ed5e2bfacdde3273fc2c5b837adc4e8f5b0
woshiLileilei <103889919+woshiLileilei@users.noreply.github.com>
2023-11-05 18:47:16 +0800

    Deleted useless routers

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,14 @@
 import {createRouter, createWebHashHistory, createWebHistory} from "vue-router";
-
 import StartPage from "@/StartPage.vue";
 import ModeChoose from "@/ModeChoose.vue";
 import WareHouse from "@/WareHouse.vue";
-
 import GameInterface from "@/Gameview/GameInterface.vue";
 import Game from "@/Game.vue";
 import GameEquipment from "@/Gameview/Equipment.vue";
-
 import Catalog from "@/views/Catalog.vue";
-import ProductDetail from "@/views/ProductDetail.vue";
 import ProductDetailUser from "@/views/ProductDetailUser.vue";
-import Cart from "@/views/Cart.vue";
-import CatalogAdmin from "@/views/CatalogAdmin.vue";
-import Edit from "@/views/Edit.vue";
+
+
 
 
 const router = createRouter({
@@ -25,59 +20,40 @@ const router = createRouter({
             component: StartPage
         },
         {
-            path: "/ModeChoose/:userid",
+            path: "/ModeChoose/:username",
             name:"ModeChoose",
             component: ModeChoose,
         },
         {
-            path: "/WareHouse/:userid",
+            path: "/WareHouse/:username",
             name:"WareHouse",
             component: WareHouse,
         },
         {
-            path: "/Game/:userid",
+            path: "/Game",
             name: Game,
             component: Game,
         },
         {
-            path: "/GameInterface/:gameinfo",
+            path: "/GameInterface",
             name: GameInterface,
             component: GameInterface,
         },
         {
-            path:"/GameEquipment/:difflv",
+            path:"/GameEquipment",
             name:GameEquipment,
             component:GameEquipment
         },
-        {
-            path: '/CatalogAdmin',
-            name: 'CatalogAdmin',
-            component: CatalogAdmin
-        },
-        {
-            path: '/Catalog/product/:id',
-            name: 'ProductView',
-            component: ProductDetail
-        },
+
         {
             path: '/Catalog/productUser/:id',
             name: 'ProductViewUser',
             component: ProductDetailUser
         },
         {
-            path: '/Catalog/cart',
-            name: 'CartView',
-            component: Cart
-        },
-        {
             path: '/Catalog/',
             name: 'Catalog',
             component: Catalog
-        },
-        {
-            path: '/Catalog/Edit',
-            name: 'EditView',
-            component: Edit
         },
         {
             path: '/Catalog/ProductDetailUser',
